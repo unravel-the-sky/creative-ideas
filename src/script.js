d89f7be7b191f0482a81a7e917305f0f07b2e4bd
unravel-the-sky <sekdemir@gmail.com>
2024-07-23 14:21:25 +0200

    fishesss

--- src/script.js
+++ src/script.js
@@ -2,13 +2,17 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui';
 import gsap from 'gsap'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+const gltfLoader = new GLTFLoader();
 
 /**
  * Base
  */
 const params = {
     mainColor: '#1e665a',
-    cameraPos: 5
+    background: '#5199db',
+    cameraPos: 8
 };
 
 const material = new THREE.MeshStandardMaterial(
@@ -23,6 +27,10 @@ gui.addColor(params, 'mainColor').onChange((color) => {
     console.log(color)
     material.color.set(color)
 })
+gui.addColor(params, 'background').onChange((color) => { 
+    console.log(color)
+    scene.background.set(color)
+})
 gui.add(params, 'cameraPos').min(0).max(15).step(0.01).onChange((val) => {
     camera.position.z = val;
 })
@@ -32,12 +40,54 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(params.background)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
 const texture = textureLoader.load('/textures/particles/8.png')
+
+/**
+ * Load models
+ */
+let nemoFish = null
+let nemoMixer = null
+const model = gltfLoader.load('models/nemo.glb', 
+    (glb) => {
+        console.log(glb)
+        nemoFish = glb.scene;
+        
+        nemoMixer = new THREE.AnimationMixer(glb.scene);
+        const action = nemoMixer.clipAction(glb.animations[0])
+        console.log(action)
+
+        action.play();
+
+        glb.scene.scale.setScalar(0.3)
+        glb.scene.position.set(0, 0, -100)
+        scene.add(glb.scene)
+    }
+)
+
+let bigFish = null
+let bigFishMixer = null
+const bigFishModel = gltfLoader.load('models/bigFish.glb', 
+    (glb) => {
+        console.log(glb)
+        bigFish = glb.scene;
+        
+        bigFishMixer = new THREE.AnimationMixer(glb.scene);
+        const action = bigFishMixer.clipAction(glb.animations[0])
+        console.log(action)
+
+        action.play();
+
+        glb.scene.scale.setScalar(0.4)
+        glb.scene.position.set(0, 0, -200)
+        scene.add(glb.scene)
+    }
+)
 
 /**
  * Test cube
@@ -47,7 +97,7 @@ const cube = new THREE.Mesh(
     material
 )
 cube.position.set(0, 0, -400)
-scene.add(cube)
+// scene.add(cube)
 
 const torus = new THREE.Mesh(
     new THREE.TorusGeometry(0.9),
@@ -58,7 +108,7 @@ torus.scale.set(0.5, 0.5, 0.5)
 // scene.add(torus)
 
 const numParticles = 300;
-const rangeParticles = 10;
+const rangeParticles = params.cameraPos * 2;
 const particlesPos = new Float32Array(numParticles * 3)
 for(let i = 0; i < numParticles * 3 ; i++) {
     const i3 = i * 3;
@@ -124,29 +174,25 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 6
+camera.position.z = params.cameraPos
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 // controls.
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
 
-scene.add(new THREE.AxesHelper(2))
+// scene.add(new THREE.AxesHelper(2))
 
 const cursor = {
     x: 0,
@@ -173,21 +219,45 @@ window.addEventListener('mousemove', (event) => {
             // cube.lookAt(new THREE.Vector3(cursor.x, cursor.y, 0))
         }
     })
-    // gsap.to(torus.position, {
-    //     x: cursor.x,
-    //     y: cursor.y,
-    //     duration: 0.4,
-    //     // delay: 0.5,
-    //     // ease: 'power2.inOut'
-    //     onComplete: () => {
-    //         torus.lookAt(camera.position)
-    //     }
-    // })
+    if (nemoFish) {
+        gsap.to(nemoFish.position, {
+            x: Math.sin(cursor.x) + cursor.x,
+            y: cursor.y - 1.2,
+            z: 0,
+            duration: 0.4,
+            // delay: 0.5,
+            // ease: 'power2.inOut'
+            onComplete: () => {
+                nemoFish.lookAt(new THREE.Vector3(cursor.x, cursor.y, camera.position.z - 5))
+            }
+        })
+    }
+    if (bigFish) {
+        gsap.to(bigFish.position, {
+            x: Math.sin(cursor.x) + cursor.x,
+            y: cursor.y,
+            z: -4,
+            duration: 0.4,
+            // delay: 0.5,
+            // ease: 'power2.inOut'
+            onComplete: () => {
+                bigFish.lookAt(new THREE.Vector3(cursor.x, cursor.y, camera.position.z - 5))
+            }
+        })
+    }
 })
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let currentTime = 0;
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - currentTime;
+    currentTime = elapsedTime;
 
     // for (let i = 0; i < size ; i++) {
     //     const i3 = i * 3;
@@ -196,7 +266,13 @@ const tick = () =>
     //     // console.log(Math.sin(elapsedTime))
     // }
 
-    
+    if (nemoMixer) {
+        nemoMixer.update(deltaTime)
+    }
+
+    if (bigFishMixer) {
+        bigFishMixer.update(deltaTime)
+    }
 
     // Update controls
     // controls.update()
